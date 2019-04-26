093b468268f37313a0709541ff81030975b758a6
maclevine <49597495+maclevine@users.noreply.github.com>
2019-04-26 23:31:45 +0200

    Add files via upload

--- accueil.php
+++ accueil.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-$bdd = new PDO("mysql:host=sql310.epizy.com;dbname=","","");
+$bdd = new PDO("mysql:host=sql310.epizy.com;dbname=epiz_23760423_bdd","epiz_23760423","AoOnbuefkx7");
 
 	if(isset($_GET['id']) AND $_GET['id'] > 0)
 	{
@@ -17,7 +17,8 @@ $bdd = new PDO("mysql:host=sql310.epizy.com;dbname=","","");
 		$insertmsg = $bdd -> prepare('INSERT INTO chat(msg) VALUES(?)');
 		$insertmsg -> execute(array($msg));
 	}
-
+	$insertpseudo = $bdd -> prepare('SELECT * FROM chat WHERE pseudo = ?');
+	$insertpseudo -> execute(array($info_utilisateur['pseudo']));
 
 	if(isset($_SESSION['id']) AND $info_utilisateur['id'] == $_SESSION['id'])
 	{	
@@ -51,38 +52,37 @@ $bdd = new PDO("mysql:host=sql310.epizy.com;dbname=","","");
 	<h1 class="vw10 bleufonce ombre police1 aligntitre"><a href="accueil.php?id=<?php echo $info_utilisateur['id']; ?>" class="anoneh1">Time<span class="bleuclair">2</span>Chat</a></h1>
 </div>
 		</div>
-			<div id="message">
-				<div align="center">
-			<?php
-
-			$tous_les_msg =  $bdd -> query('SELECT * FROM chat ORDER BY id DESC LIMIT 0, 5');
-			while($msg = $tous_les_msg -> fetch())
-			{
-			?>
-			<div align="center">
-				<div style="width: 25%; background-color: red;">
-					<div align="left">
-				<p><?php if(isset($_POST['pseudo'])){ echo $_POST['pseudo']; } ; ?></b> : <?php echo $msg['msg']; ?></p>
-					</div>
-				</div>
-			</div>
-		</div>
-			<?php
-			}
-			?>
-		</div>
-
+		<div style="border: 1px solid red; background-color: white; width: 50%; height: 50%;margin-left: 25%; overflow: scroll; overflow-x: hidden;">
+								<div id="message">
+								
+								<?php 
+								$tous_les_msg =  $bdd -> query('SELECT * FROM chat ORDER BY id ');
+								while($msg = $tous_les_msg -> fetch())
+								{
+								?>
+									<div align="left">
+									<p><?php if(!empty($info_utilisateur['avatar'])) {?><img src="avatars/<?php  echo $info_utilisateur['avatar']; ?>" width="50" height="50"> <?php }?> <?php echo $info_utilisateur['pseudo'] ; ?> : <?php echo $msg['msg']; ?></p>
+									</div>
+								<?php
+								}
+								?>
+							</div>
+</div>
 					<div align="center">
-				<form method="post" action="">
-						<textarea type="text" name="msg" placeholder="MESSAGE"></textarea>
+				<form method="post" action="" name="chat">
+						<input type="text" name="msg" placeholder="Envoyer un message" autocomplete="off">
 						<input type="submit" name="" value="Envoyer">
 
 				</form>
 <script type="text/javascript">
-	setInterval('chargement_message()', 1500);
+	setInterval('chargement_message()', 500);
 	function chargement_message(){
 		$('#message').load('chargement_message.php');
 	}
+</script>
+
+<script type="text/javascript">
+	document.chat.msg.focus();
 </script>
 
 <?php
--- chargement_message.php
+++ chargement_message.php
@@ -1,18 +1,16 @@
-				<div align="center">
-			<?php
-			$bdd = new PDO("mysql:host=sql310.epizy.com;dbname=","","");
-			$tous_les_msg =  $bdd -> query('SELECT * FROM chat ORDER BY id DESC LIMIT 0, 5');
-			while($msg = $tous_les_msg -> fetch())
-			{
-			?>
-			<div align="center">
-				<div style="width: 25%; background-color: red;">
-					<div align="left">
-				<p><?php if(isset($_POST['pseudo'])){ echo $_POST['pseudo']; } ?></b> : <?php echo $msg['msg']; ?></p>
-					</div>
-				</div>
-			</div>
-		</div>
-			<?php
-			}
-			?>
+<div style="border: 1px solid red; background-color: white;">
+								<div id="message">
+								
+								<?php 
+								$tous_les_msg =  $bdd -> query('SELECT * FROM chat ORDER BY id LIMIT 0, 7');
+								while($msg = $tous_les_msg -> fetch())
+								{
+								?>
+									<div align="center">
+									<p><?php if(!empty($info_utilisateur['avatar'])) {?><img src="avatars/<?php  echo $info_utilisateur['avatar']; ?>" width="50" height="50"> <?php }?> <?php echo $info_utilisateur['pseudo'] ; ?> : <?php echo $msg['msg']; ?></p>
+									</div>
+								<?php
+								}
+								?>
+							</div>
+</div>
